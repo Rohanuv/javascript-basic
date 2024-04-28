1d30ac697bb53a4b8ec3d9a9bebb5891ce75b5e1
Rohanuv <33950153+Rohanuv@users.noreply.github.com>
2024-04-28 12:53:14 +0000

    container config done

---
+++ Basic/Varible.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountEmail = "Rohan@google.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+// accountId = 2  // not allowed
+
+accountEmail = "rohan@db.com"
+accountPassword = 212345
+accountCity = "Pune"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])

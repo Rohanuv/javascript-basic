06aa0fed079829693e17493d77cf98e35389cdc6
Rohanuv <33950153+Rohanuv@users.noreply.github.com>
2024-05-08 14:37:59 +0000

    container config

---
+++ Basic/string.js
@@ -0,0 +1,15 @@
+const name = "Rohan"
+const repoCount = 50;
+
+//console.log(name + repoCount + "Value")
+
+//console.log(`Hello My name is ${name} and my repoCount is ${repoCount}`)
+
+const gameName = new String("rohanuv")
+console.log(gameName)
+
+//console.log(gameName.toLocaleUpperCase())
+
+//console.log(gameName.toUpperCase())
+
+console.log(gameName.charAt(3))

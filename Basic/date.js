4babc9bed59916402b2b9a9f5d671169c1456772
Rohanuv <33950153+Rohanuv@users.noreply.github.com>
2024-05-11 14:39:41 +0000

    container config

---
+++ Basic/date.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+
+console.log(myDate)
+
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toLocaleTimeString("en-In"));

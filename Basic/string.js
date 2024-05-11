37eed4a059dbadfd090f9d46e61ef119ce2d4968
Rohanuv <33950153+Rohanuv@users.noreply.github.com>
2024-05-11 06:51:54 +0000

    string

--- Basic/string.js
+++ Basic/string.js
@@ -2,3 +2,28 @@ const name = "Rohan"
 
 const repoCount = 100
 console.log(name + repoCount)
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+ const gameName = new String("rohanuv")
+// console.log(gameName.length)
+
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt(3));
+
+// console.log(gameName.indexOf('u'));
+
+
+ const newString = gameName.substring(3,5)
+
+ console.log(newString)
+
+const anotherString = gameName.slice()
+
+console.log(anotherString)
+
+const newStringOne = "       RoHaN      "
+console.log(newStringOne)
+
+console.log(newStringOne.trim())

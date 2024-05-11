59d424a181051bab3f58f02cbc2493227bef1099
Rohanuv <33950153+Rohanuv@users.noreply.github.com>
2024-05-11 13:56:06 +0000

    math

--- Basic/math.js
+++ Basic/math.js
@@ -1,3 +1,30 @@
 const score = 67
+// console.log(score)
+// const balance = new Number(400)
 
-const balance = new Number(400)
+// console.log(balance.toString().length)
+
+// console.log(typeof balance)
+
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 123.678
+
+// console.log(otherNumber.toPrecision(3))
+
+// const hundred = 100000
+
+// console.log(hundred.toLocaleString('en-In'))
+
+
+//MATH
+
+console.log(Math)
+
+// console.log(Math.abs(-20))
+
+// console.log(Math.round(4.5))
+
+console.log(Math.ceil(4.1))
+
+console.log(Math.floor(4.9))

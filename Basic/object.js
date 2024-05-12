5991f2e3723d854d5a759cc46c7fe55cf0a96214
Rohanuv <33950153+Rohanuv@users.noreply.github.com>
2024-05-12 17:08:33 +0000

    contanier config

--- Basic/object.js
+++ Basic/object.js
@@ -1,11 +1,36 @@
 // Object literal
 
+const mySym = Symbol("key1")
+
 const JsUser = {
     name : "Rohan",
+    "Full name": "Rohan Sonavne",
+    [mySym] : "myKey1",
     age :38,
     location: "Mumbai",
     email:"rohan@google.com",
     isLoggedIn: false,
     lastLoginDays:["Monday", "Saturday"]
-
 }
+
+// console.log(JsUser["email"])
+// console.log(JsUser["Full name"])
+
+// console.log(JsUser)
+
+// JsUser.email= "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+// JsUser.email= "hitesh@micro.com"
+
+// console.log(JsUser)
+
+JsUser.greeting = function() {
+    console.log("Hello JS User")
+}
+
+JsUser.greetingA = function() {
+    console.log(`Hello JS User ${this.age}`)
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingA());

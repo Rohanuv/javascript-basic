6ee6f9483095eb624282a86395b58eefdbf052a3
Rohanuv <33950153+Rohanuv@users.noreply.github.com>
2024-05-12 05:18:10 +0000

    container config

--- Basic/date.js
+++ Basic/date.js
@@ -1,11 +1,28 @@
 let myDate = new Date()
+// console.log(myDate.toDateString());
 
-console.log(myDate)
+// console.log(myDate.toISOString());
 
-console.log(myDate.toString());
+// console.log(myDate.toJSON());
 
-console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
 
-console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
 
-console.log(myDate.toLocaleTimeString("en-In"));
+// console.log(myDate.toLocaleTimeString());
+
+// let myCreateDate = new Date(1987, 9, 27);
+
+// console.log(myCreateDate.toDateString());
+
+// console.log(myCreateDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+
+// console.log (Math.floor(Date.now()/1000));
+
+  
+ 
+

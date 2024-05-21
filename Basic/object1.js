1cf9cb5a78d96e0de2895f5904d84332e458f5b6
Rohanuv <33950153+Rohanuv@users.noreply.github.com>
2024-05-21 07:11:24 +0000

    container config

---
+++ Basic/object1.js
@@ -0,0 +1,11 @@
+const Jsuser = {
+    name = "Rohan",
+    age:37,
+    location:"Karad",
+    email:"rohan@google.com",
+    isLoggedIn: False,
+    lastLoginIn:["Monday", "Sunday"]
+}
+
+console.log(Jsuser.email)
+console.log(Jsuser["email"])

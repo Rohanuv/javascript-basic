c44ea42f07194dc8a522e2ba94688d045f246691
Rohanuv <33950153+Rohanuv@users.noreply.github.com>
2024-05-23 07:29:42 +0000

    function

---
+++ Basic/function.js
@@ -0,0 +1,26 @@
+
+
+function sayMyName(){
+console.log("H");
+console.log("A");
+console.log("N");
+}
+
+//sayMyName()
+
+// function addTwoNumber(num1, num2){
+//    console.log(num1 + num2); 
+
+// }
+
+function addTwoNumber(num1, num2){
+    let result = num1 + num2 
+
+    return result;
+ 
+ }
+ 
+
+const result = addTwoNumber(3,5)
+
+//console.log("Result", result);

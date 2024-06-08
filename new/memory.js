1be30bc2f96b283fed9272fc933c41e99e22d09c
Rohanuv <33950153+Rohanuv@users.noreply.github.com>
2024-06-08 13:43:11 +0000

    memory

---
+++ new/memory.js
@@ -0,0 +1,24 @@
+let myYouTube = "Rohan"
+
+let anotherName = myYouTube
+
+anotherName = "chaiaurCode"
+
+
+console.log(anotherName);
+console.log(myYouTube);
+
+let UserOne = {
+    email:"use@google.com",
+    upi:"user@123"
+}
+
+let UserTwo = UserOne
+
+UserTwo.email= "rohanuv@gmail.com"
+
+console.log(UserOne);
+console.log(UserTwo);
+
+
+
